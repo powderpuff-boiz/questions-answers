a3747eb880ca242b75e793a30eaf14bd169366cc
Michelle Kim <michellekimx88@gmail.com>
2021-10-26 20:28:28 -0700

    Finished pseudocode for models and controllers

--- models/models.js
+++ models/models.js
@@ -1,5 +1,7 @@
 const { Question, Answer, Photo } = require('../database/schema.js');
 
+// ====== QUESTIONS ====== //
+
 const q = {
   get: (params) => {
     // query to grab questions by product ID
@@ -17,30 +19,43 @@ const q = {
   helpful: (params) => {
     // query to update question helpfulness
     // grab question ID
-    //
+    // findOneAndUpdate - helpfulness + 1
   },
   report: (params) => {
     // query to report question
+    // grab question ID
+    // findOneAndUpdate - reported = 1
   }
-
 };
+
+
+
+// ====== ANSWERS ====== //
 
 const a = {
   get: (params) => {
-
+    // query to grab answers for specific question
+    // grab questionID, page, count
+    // match by question ID
+    // sort by id: 1
+    // limit to page and count
   },
   post: (params) => {
-
+    // query to create new answer
+    // grab question ID
+    // get answer data and apply answer model
+    // Answer.create(answer data)
   },
   helpful: (params) => {
-
+    // query to update answer helpfulness
+    // grab answer ID
+    // findOneAndUpdate - helpfulness + 1
   },
   report: (params) => {
-
+    // query to report answer
+    // grab answer ID
+    // findOneAndUpdate - reported = 1
   }
 };
 
-module.exports = {
-  q: q,
-  a: a
-};
+module.exports = { q: q, a: a };

47b89291a1b6e1d218cb216a6a3c68b5d6661b30
Michelle Kim <michellekimx88@gmail.com>
2021-11-19 12:56:48 -0800

    Ready for testing

--- database/index.js
+++ database/index.js
@@ -10,8 +10,8 @@ const answersCSV = '/Users/michellekim/rpp30/questions-answers/database/answers.
 const photosCSV = '/Users/michellekim/rpp30/questions-answers/database/answers_photos.csv';
 
 //mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connect('mongodb://mkim:HRadmin@ec2-3-84-180-117.compute-1.amazonaws.com/QnA?authSource=admin', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb://localhost/QnA', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://mkim:HRadmin@ec2-54-82-63-143.compute-1.amazonaws.com/QnA?authSource=admin', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost/QnA', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 

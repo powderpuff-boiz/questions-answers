a2ec27e43f2404df1d9bedc8dc5352be47955696
Michelle Kim <michellekimx88@gmail.com>
2021-11-19 13:08:36 -0800

    Commented out config file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const config = require('../config.js');
+// const config = require('../config.js');
 const express = require('express');
 const router = require('./router.js');
 const path = require('path');
